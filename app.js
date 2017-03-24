76fc50d470d2fd6e286f14b726c90a2ad8fe3340
AgataKondzior <ak.kondzior@gmail.com>
2017-03-24 09:04:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var main = function (){
    $(".menu-toggle").click(function(){
     $(".menu").animate({
             left:"0px"
-        },600);      
+        },200);      
 
    $("body").animate({
             left:"180px"
@@ -20,7 +20,7 @@ var main = function (){
 
    $("body").animate({
             left:"0px"
-        },600);
+        },200);
    $(".menu-toggle").show(100);
 });
   };
